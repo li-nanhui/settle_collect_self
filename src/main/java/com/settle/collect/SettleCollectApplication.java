9b14ae54804534a51b76ecb9b852bb52cc390136
li_nanhui <li_nanhui@163.com>
2022-11-02 19:57:52 +0800

    初始化

---
+++ src/main/java/com/settle/collect/SettleCollectApplication.java
@@ -0,0 +1,13 @@
+package com.settle.collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleCollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettleCollectApplication.class, args);
+    }
+
+}
